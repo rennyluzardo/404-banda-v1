3e9e1da8310902b180d6cef030ad44a3f875bb8a
Renny Luzardo <rennyjoseluzardocolina@gmail.com>
2023-06-20 04:08:32 -0500

    Info about page in construction

--- app/page.js
+++ app/page.js
@@ -6,8 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+          ¡Estamos en contrucción!
         </p>
         <div>
           <a
