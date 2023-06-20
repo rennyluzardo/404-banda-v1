2b332af75eb04ba79c000a1225d1c5c1ae4ad5f3
Renny Luzardo <rennyjoseluzardocolina@gmail.com>
2023-06-20 13:00:45 -0500

    Custom app

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// import MainLayout from '../components/global/MainLayout'
+import '../scss/styles.scss'
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
