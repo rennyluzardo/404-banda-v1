7c2b54969fb7eace010347cc2c2db8d3d0171a1c
Renny Luzardo <rennyjoseluzardocolina@gmail.com>
2023-06-20 12:59:55 -0500

    Main layout base structure

---
+++ app/components/global/MainLayout.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { Inter } from 'next/font/google'
+import { Layout, Space, Button, Row, Col, } from 'antd'
+const { Header, Footer, Content } = Layout
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: '404 Banda',
+  description: 'Haciendo musica desde el 2023',
+}
+
+const headerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 64,
+  paddingInline: 50,
+  lineHeight: '64px',
+  backgroundColor: '#7dbcea',
+}
+const contentStyle = {
+  textAlign: 'center',
+  minHeight: 120,
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#108ee9',
+}
+const footerStyle = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#7dbcea',
+}
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className}>{children}</body>
+//     </html>
+//   )
+// }
+
+
+export default function MainLayout({ children }) {
+  return (
+    <Layout>
+      <Header style={headerStyle}>
+        <Row>
+          <Col span={12}>
+            <Space align="start" style={{ width: '100%' }}>
+              <p>404 BANDA</p>
+            </Space>
+          </Col>
+          <Col span={12}>
+            <Space
+              direction='horizontal'
+              align='end'
+              style={{ width: '100%', justifyContent: 'flex-end' }}
+              
+            >
+              <a href='#videos'>Vídeos</a>
+              <a href='#musica'>Música</a>
+              <a href='#letras'>Letras</a>
+              <a href='#'>Contacto</a>
+            </Space>
+          </Col>
+        </Row>
+
+      </Header>
+      <Content style={contentStyle}>
+        {children}
+      </Content>
+      <Footer style={footerStyle}>Hecho con <span className="heart"></span> por Ing. Renny Luzardo. Todos los derechos reservados 2023.</Footer>
+    </Layout>
+  )
+}
